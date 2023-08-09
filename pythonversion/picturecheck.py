14de87b0824d154f3f1d311199d4e1b4f1ab114c
nowiley <122295570+nowiley@users.noreply.github.com>
2023-08-09 14:48:21 -0700

    More checks for Yolo check
    Now checks that there is confidently only 1 person and that person takes up at least 80% of image by height:
    
    Output tuple now includes more data for debugging

--- pythonversion/picturecheck.py
+++ pythonversion/picturecheck.py
@@ -6,34 +6,60 @@ from imageanalysis import dict_to_body_vector
 #Using nano model for faster detection
 GLOBAL_IMGCHECK_MODEL = YOLO("yolov8n.pt")
 
+from ultralytics import YOLO
+
+#Using nano model for faster detection
+GLOBAL_IMGCHECK_MODEL = YOLO("yolov8n.pt")
+
+# Basically: Ensure image has 1 person, and that person takes up most of the image by height
+# provides extra data for debugging
 def image_check(image_path):
   """
-  Checks for poorly formatted images or images that are not humans 
+  Checks for poorly formatted images or images that are not humans
   using YOLO object detection and comparing box to total image size
   Input: input image
-  Output: (True, confidence level)/(False, -1) if valid person detected/not detected
+  Output: (T/F, # People in frame, max pheight/imgheight, confidence level of max
+  p/img, (x1,y1, x2, y2))
   """
   # Run model
   res = GLOBAL_IMGCHECK_MODEL.predict(image_path)[0]
   # List of tuples (x1, y1, x2, y2, conf, obj type)
   detected_objects = res.boxes.data
-  #Orig height to compare 
+  # Orig height to compare
   orig_height = res.orig_shape[0]
+  # Counters and Trackers
+  ppl_in_frame = 0
+  max_ratio = -1
+  conf_of_max_ratio = -1
+  b_box_of_max_ratio = (-1,-1,-1,-1)
 
+  # Loop through all objects
   for obj in detected_objects:
-    # If person object not detected (obj[5] != 0)
+    # If person object not detected (obj[5] != 0) continue
     if obj[5] != 0:
       continue
+
+    #Increment person counter:
+    ppl_in_frame += 1
 
     #Get height of detection box & conf level
     person_height = obj[1] - obj[3]
     conf = obj[4]
 
-    if abs(person_height/orig_height) >= 0.8 and (conf > 0.5):
-      return (True, float(conf))
+    # Update max person ratio stats
+    cur_person_ratio = abs(person_height/orig_height)
+    if cur_person_ratio > max_ratio:
+      max_ratio = cur_person_ratio
+      conf_of_max_ratio = conf
+      b_box_of_max_ratio = tuple([float(coord) for coord in obj[0:4]])
 
-  #If no people objects detected
-    return (False, -1)
+
+  # If != 1 person detected or max_ratio < 80%, not a valid image
+  good_image = True
+  if ppl_in_frame != 1 or max_ratio < 0.8 or conf_of_max_ratio < 0.6:
+    good_image = False
+  
+  return (good_image, ppl_in_frame, float(max_ratio), float(conf_of_max_ratio), b_box_of_max_ratio)
 
 
 # Basic person check make sure all dimensions are reasonable
